07c73728d37c62588035b2a4b51bbc29bedc6727
lhangnavig <lhangnavig@163.com>
2018-09-30 13:24:42 +0800

    new

---
+++ src/main/java/com/yyq/es_retreatment/EsRetreatmentApplication.java
@@ -0,0 +1,12 @@
+package com.yyq.es_retreatment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsRetreatmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EsRetreatmentApplication.class, args);
+    }
+}
